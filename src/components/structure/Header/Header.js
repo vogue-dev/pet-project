7fd23ee1bf94cafa10ddc4197915e338c65615b6
Vogue <oprya1111@yandex.ru>
2020-09-06 02:15:51 +0300

    initial commit

--- src/components/structure/Header/Header.js
+++ src/components/structure/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Header.css';
+import './Header.scss';
 import ModalRight from '../Modals/ModalRight/ModalRight.js';
 import Logo from '../Logo/Logo.js';
 
@@ -23,6 +23,25 @@ export default class Header extends Component {
 						) : (
 							<span onClick={() => this.props.handleClick(3)}>Login</span>
 						)}
+					</div>
+					<div className="header__mobile">
+						<div className="header__mobile--button" onClick={() => this.props.toggleModalRight()}>
+							<svg
+								viewBox="0 0 24 24"
+								fill="white"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								class="feather feather-list">
+								<line x1="8" y1="6" x2="21" y2="6"></line>
+								<line x1="8" y1="12" x2="21" y2="12"></line>
+								<line x1="8" y1="18" x2="21" y2="18"></line>
+								<line x1="3" y1="6" x2="3" y2="6"></line>
+								<line x1="3" y1="12" x2="3" y2="12"></line>
+								<line x1="3" y1="18" x2="3" y2="18"></line>
+							</svg>
+						</div>
 					</div>
 
 					<ModalRight
